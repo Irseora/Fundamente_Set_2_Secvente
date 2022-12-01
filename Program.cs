656b518f4748bf72fd36886d885e6c319f158ec8
Irseora <irseora@gmail.com>
2022-12-01 16:30:05 +0200

    Indicatii

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Set2
         {
             Console.Clear();
             for (int i = 0; i < indicatii.Length; i++)
-                Console.WriteLine($"{indicatii[i]}{Environment.NewLine}");
+                Console.WriteLine($"{indicatii[i]}");
             
             int prob = 0;
             while (prob < 1)
@@ -47,16 +47,23 @@ namespace Set2
                                             // Exemplu: 0, 1, 1, 2, 3, 5, 8 ...
                                    " 9. Sa se determine daca o secventa de n numere este monotona.",
                                             // Secventa monotona = secventa monoton crescatoare sau monoton descrescatoare.
-                                  $"10. Se da o secventa de n numere.{Environment.NewLine}    Care este numarul maxim de numere consecutive egale din secventa.",
+                                  $"10. Se da o secventa de n numere.{Environment.NewLine}    Care este numarul maxim de numere consecutive egale din secventa?",
                                   $"11. Se da o secventa de n numere.{Environment.NewLine}    Se cere sa se caculeze suma inverselor acestor numere.",
-                                  $"12. Cate grupuri de numere consecutive diferite de zero sunt intr-o secventa de n numere.{Environment.NewLine}    Considerati fiecare astfel de grup ca fiind un cuvant, zero fiind delimitator de cuvinte.",
+                                  $"12. Cate grupuri de numere consecutive diferite de zero sunt intr-o secventa de n numere.",
+                                            // Considerati fiecare astfel de grup ca fiind un cuvant, zero fiind delimitator de cuvinte.
                                             // De ex. pentru secventa 1, 2, 0, 3, 4, 5, 0, 0, 6, 7, 0, 0 raspunsul este 3.
-                                  $"13. O <secventa crescatoare rotita> este o secventa de numere care este in ordine crescatoare{Environment.NewLine}    sau poate fi transformata intr-o secventa in ordine crescatoare prin rotiri succesive.{Environment.NewLine}    Determinati daca o secventa de n numere este o secventa crescatoare rotita.",
+                                  $"13. Determinati daca o secventa de n numere este o secventa crescatoare rotita.",
+                                            // O <secventa crescatoare rotita> este o secventa de numere care este in ordine crescatoare sau poate fi transformata
+                                            // intr-o secventa in ordine crescatoare prin rotiri succesive.    
                                             // Rotire cu o pozitie spre stanga = toate elementele se muta cu o pozitie spre stanga & primul element devine ultimul
-                                  $"14. O <secventa monotona rotita> este o secventa de numere monotona{Environment.NewLine}    sau poate fi transformata intr-o secventa montona prin rotiri succesive.{Environment.NewLine}    Determinati daca o secventa de n numere este o secventa monotona rotita.",
-                                  $"15. O secventa bitonica este o secventa de numere care incepe monoton crescator si continua monoton descrecator.    Se da o secventa de n numere. Sa se determine daca este bitonica.",
+                                  $"14. Determinati daca o secventa de n numere este o secventa monotona rotita.",
+                                            // O <secventa monotona rotita> este o secventa de numere monotona sau poate fi transformata intr-o secventa montona
+                                            // prin rotiri succesive.    
+                                  $"15. Se da o secventa de n numere. Sa se determine daca este bitonica.",
+                                            // O secventa bitonica este o secventa de numere care incepe monoton crescator si continua monoton descrecator.
                                             // De ex. 1,2,2,3,5,4,4,3 este o secventa bitonica.
-                                  $"16. O <secventa bitonica rotita> este o secventa bitonica{Environment.NewLine}    sau una ca poate fi transformata intr-o secventa bitonica prin rotiri succesive.{Environment.NewLine}    Se da o secventa de n numere. Se cere sa se determine daca este o secventa bitonica rotita.",
+                                  $"16. Se da o secventa de n numere. Se cere sa se determine daca este o secventa bitonica rotita.",
+                                            // O <secventa bitonica rotita> este o secventa bitonica sau una care poate fi transformata intr-o secventa bitonica prin rotiri succesive.
                                             // Rotire = primul element devine ultimul
                                   $"17. Se da o secventa de 0 si 1, unde 0 inseamna paranteza deschisa si 1 inseamna paranteza inchisa.{Environment.NewLine}    Determinati daca secventa reprezinta o secventa de paranteze corecta si,{Environment.NewLine}    daca este, determinati nivelul maxim de incuibare a parantezelor.",
                                             // De exemplu 0 1 0 0 1 0 1 1 este corecta si are nivelul maxim de incuibare 2, pe cand 0 0 1 1 1 0 este incorecta.
